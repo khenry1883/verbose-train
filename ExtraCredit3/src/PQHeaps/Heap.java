1ed1702cdc6975b261891d26195044be0901d1be
Kareena Henry <supergirl.henry@gmail.com>
2020-05-03 13:18:38 -0700

    Modified comparison symbols and names

--- ExtraCredit3/src/PQHeaps/Heap.java
+++ ExtraCredit3/src/PQHeaps/Heap.java
@@ -9,7 +9,7 @@ public class Heap {
 	private int n; // number of elements
 
 	public Heap() {
-		a = new int[1];
+		a = new int[2];
 		n = 0;
 		a[0] = 100;
 	}
@@ -29,13 +29,15 @@ public class Heap {
 	private void upHeap(int i) {
 		int k = a[i];
 		int iparent = i / 2;
-		while (a[iparent] < k) // while parent's value is less than child's value
-		{
-			a[i] = a[iparent]; // child value becomes parent's value
-			i = iparent; // child key becomes parent key
-			iparent = i / 2; // parent key decreases by half
+		while (a[iparent] > k) {
+			a[i] = a[iparent];
+			i = iparent;
+			iparent = i / 2;
+			if (iparent == i) {
+				break;
+			}
 		}
-		a[i] = k; // child gets original last val
+		a[i] = k;
 	}
 
 	public int removeMax() {
@@ -55,7 +57,7 @@ public class Heap {
 		int right = left + 1;
 
 		while (right <= n) {
-			if (k >= a[left] && k >= a[right]) {
+			if (k <= a[left] && k <= a[right]) {
 				a[i] = k;
 				return;
 			} else if (a[left] > a[right]) {
@@ -81,11 +83,11 @@ public class Heap {
 		int left = 2 * i;
 		int right = 2 * i + 1;
 		int largest;
-		if (left >= N && Arr[left] > Arr[i])
+		if (left <= N && Arr[left] < Arr[i])
 			largest = left;
 		else
 			largest = i;
-		if (right >= N && Arr[right] > Arr[largest])
+		if (right <= N && Arr[right] < Arr[largest])
 			largest = right;
 		if (largest != i) {
 			int temp = Arr[i];
@@ -97,8 +99,8 @@ public class Heap {
 
 	public void printArray() {
 		System.out.print("{");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + ", ");
+		for (int i = 0; i <= n; i++) {
+			System.out.print(a[i] + " ");
 		}
 		System.out.print("}");
 	}
@@ -106,22 +108,21 @@ public class Heap {
 	public static class DriverClass {
 		public static void main(String[] args) {
 			Heap heap = new Heap();
-			heap.insertItem(200); // Insert item into Heap
+			heap.insertItem(500); // Insert item into Heap
 			heap.removeMax();// Delete Max from the Heap
 			heap.printArray(); // Display the Heap array
 
 			// Convert user-input array to Heap
-			int num = 0;
-			Heap heap2 = new Heap();
+			@SuppressWarnings("resource")
 			Scanner scan = new Scanner(System.in);
-			System.out.print("Enter number of elements you want in the array: ");
-			num = scan.nextInt();
-			int a[] = new int[num];
-			System.out.println("Enter all the elements:");
-			for (int i = 0; i < num; i++) {
-				a[i] = scan.nextInt();
+			Heap heap2 = new Heap(10);
+			int b[] = new int[10];
+			System.out.println("\nEnter 2 elements:");
+			for (int i = 0; i < 2; i++) {
+				b[i] = scan.nextInt();
 			}
-			heap2.max_heapify(a, num, num);
+			heap2.max_heapify(b, 2, 2);
+			heap2.printArray();
 
 		}
 	}
